6211fade0ec91d9aefb63c50f3c33238bd53e8d7
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-26 21:10:23 +0800

    ok

--- packages/database_service/config/config.dev.js
+++ packages/database_service/config/config.dev.js
@@ -1,6 +1,6 @@
-const { PORT } = process.env
+const { PORT, DB_USERNAME, DB_PASSWORD } = process.env
 
 module.exports = {
     port: PORT || 4000,
-    mongoURI: `mongodb+srv://${}:${}@qrrice-fmwoi.azure.mongodb.net/test?retryWrites=true`
+    mongoURI: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@qrrice-fmwoi.azure.mongodb.net/test?retryWrites=true`
 }
